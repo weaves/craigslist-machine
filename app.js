d5fedaf73bd41dfc9dc8dd2e69da575786a2c327
Nick Weaver <nick.s.weaver@gmail.com>
2014-01-08 00:28:33 -0800

    GIT HUB COMMIT

--- app.js
+++ app.js
@@ -4,10 +4,12 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+
+// include packages for parsing JSON to XML and building CL XML
 var builder = require('xmlbuilder');
+var parse = require('js2xmlparser');
 
 var app = express();
 
@@ -28,17 +30,34 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+// serves main craigslist form
+app.get('/', function( req, res ) {
+	res.render('index', { title: 'Uber Craigslist Machine' });
+});
+
+// parse craigslist form to xml and send bulk post to craigslist
+app.post('/post', function( req, res ) {
+	console.log( req.body );
+	res.send();
+
+	/**
+	 * Need craigslist bulk posting credentials to begin prototyping / testing with cURL
+	 *
+	 * Once credentials are acquired, next steps are as follows:
+	 *
+	 * 1. Transform submitted JSON to craigslist XML
+	 *    XML requirements are here: http://www.craigslist.org/about/bulk_posting_interface
+	 *
+	 * 2. Submit XML to craigslist for validation
+	 *
+	 * 3. Once validated, submit XML to craigslist to post
+	 *
+	 * 4. Return response to modal and send email summary
+	 *
+	 */
+
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-/*var craig = builder.create('rdf:RDF')
-	craig.att('xmlns', 'http://purl.org/rss/1.0/')
-		.ele('channel')
-			.ele('items')
-				.ele('rdf:li', {'rdf:resource': 'JobPosting1'})
-	craig.end({ pretty: true});
-
-console.log(craig.stringify);*/
